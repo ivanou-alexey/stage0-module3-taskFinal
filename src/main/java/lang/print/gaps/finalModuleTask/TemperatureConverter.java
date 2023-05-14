60b2c61dbd233c0d8fb88f702858d8eaf191a13f
Alexey Ivanov <ai5973861@gmail.com>
2023-05-14 18:56:53 +0300

    Fix

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double a = 5 / 9;
+        double a = 9 / 5;
         double temperatureFahrenheit = temperatureCelsius * a + 32;
         System.out.println(temperatureFahrenheit);
     }
